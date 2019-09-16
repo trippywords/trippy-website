6ea4a605d2477b8cf42702f3d4932dd3d5ef13d7
chaitrali <chaitrali@addvector.com>
2019-09-16 17:05:44 +0530

    simple way to remove tags

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -105,7 +105,8 @@ endif;
 	}
 
 	// Working with featuredBlogsDetails JSON response
-	// print_r($featuredBlogsDetails);
+	//dd($featuredBlogs);
+	//dd($featuredBlogsDetails);
 ?> 
 
 	<!-- Featured Blogs Top Row -->
@@ -118,13 +119,14 @@ endif;
 							<div class="item">
 								<div>
 									<div class="blog-parent-genre">{{ $blog['parentGenre'] }}</div>
-									<img src="public/blog_img/{{ $blog['blogImg'] }}" class="blog-img">
+									 <img src="public/blog_img/{{ $blog['blogImg'] }}" class="blog-img"> 
 									<div class="blog-child-genre">{{ $blog['childGenre'] }}</div>
 									<div class="blog-title">{{ $blog['title'] }}</div>
-									<div class="blog-desc">{{ $blog['description'] }}</div>
+									<div class="blog-desc">{!! $blog['description'] !!}</div>
 								</div>
 							</div>
 						@endforeach
+
 					</div>
 					@if (count($featuredRow) > 3)
 						<button class="btn btn-primary leftLst"><</button>
