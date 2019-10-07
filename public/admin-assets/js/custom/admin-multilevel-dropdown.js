676823c3fbd813d85e855f3aad81e7cb13c04327
chaitrali <chaitrali@addvector.com>
2019-10-07 23:27:03 +0530

    js for dependent dropdown

---
+++ public/admin-assets/js/custom/admin-multilevel-dropdown.js
@@ -0,0 +1,37 @@
+//Creating multilevel dropdown dynamic
+ $(document).ready(function(){
+      
+            $('select[name="parent_genre_id"]').on('change',function(){
+                var id=$(this).val();
+                
+                if(id)
+                {
+                    //console.log(id);
+                    $.ajax({
+                        //url: ADMIN_URL+'/adminpanel/blog/ajax'+id,
+                        type:'GET',
+                        dataType:'json',
+
+                        url:ADMIN_URL+"/blog/ajax?id="+id,
+                         
+                        success:function(data)
+                        {
+                            console.log(data);
+                            $('select[name="blog_genre"]').empty();
+                            $.each(data,function(key,value){
+                            $('select[name="blog_genre"]').append('<option value="'+key+'">'+value+'</option>');
+                            
+                               });                   
+                        },
+                        error: function (e) {
+                    
+                    console.log("ERROR: ", e);
+                }
+
+                    });
+                }
+                else{
+                    $('select[name="blog_genre"]').empty();
+                }
+            });
+        });  
