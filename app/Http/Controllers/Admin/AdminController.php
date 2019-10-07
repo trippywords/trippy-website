7fd9ce9776b2c3928a702a6af28c9dd1af13916a
chaitrali <chaitrali@addvector.com>
2019-10-08 00:01:34 +0530

    admin panel dashboard error solved

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -122,12 +122,12 @@ class AdminController extends Controller
 
         $user_count=User::where('is_delete','=','0')->count();       //where('is_delete','=','0')->              
         $genre = Genre::where('is_deleted','N')->count();
-        $blogs_count = Blog::where('is_delete','0')->count();
-        $publish_blog_count=Blog::where('blog_status','=','1')->where('is_delete','0')->count();
-        $draft_blog_count=Blog::where('blog_status','=','2')->where('is_delete','0')->count();
+        $blogs_count = Blog::where('is_deleted','0')->count();
+        $publish_blog_count=Blog::where('blog_status','=','1')->where('is_deleted','0')->count();
+        $draft_blog_count=Blog::where('blog_status','=','2')->where('is_deleted','0')->count();
 
         $users=User::where('is_delete','=','0')->where('role_id','!=',4)->orderBy('id')->limit(5)->get();
-        $blogs=Blog::where('is_delete','0')->orderBy('updated_at')->limit(5)->get();
+        $blogs=Blog::where('is_deleted','0')->orderBy('updated_at')->limit(5)->get();
         $comments=Comments::getComments(5);
         $comments_count=Comments::getComments(null,true);
 
