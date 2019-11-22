35e198f185f90adaa6bf80b99ad3049629e4a0d9
chaitrali-trippy <54344515+chaitrali-trippy@users.noreply.github.com>
2019-11-22 22:49:04 +0530

    Blog detail (#36)
    * solved followers error and blog detail page for logged in and guest user
    
    * deleted ajax call for dropdown
    
    * changed blog detail for user dashboard

--- resources/views/dashboard/profile.blade.php
+++ resources/views/dashboard/profile.blade.php
@@ -57,14 +57,15 @@
                             <div class="media" data-val="<?php echo $blog->blogid; ?>">
                                 <div class="media-left">
                                     <?php if (isset($blog->blog_image) && $blog->blog_image != null && file_exists(public_path() . '/blog_img/' . $blog->blog_image)) { ?>
-                                        <a href="{{ url('blog/'.$blog->blogid) }}" target="_blank"><img src="{{ asset("/public/blog_img/".$blog->blog_image) }}" class="media-object"></a>
+                                        <a href="{{ url('blogs/'.$blog->blogid) }}" target="_blank"><img src="{{ asset("/public/blog_img/".$blog->blog_image) }}" class="media-object"></a>
                                     <?php } else { ?>
-                                        <a href="{{ url('blog/'.$blog->blogid) }}" target="_blank"><img src="{{ asset('/') }}public/blog_img/no_img.jpg" class="media-object"></a>
+                                        <a href="{{ url('blogs/'.$blog->blogid) }}" target="_blank"><img src="{{ asset('/') }}public/blog_img/no_img.jpg" class="media-object"></a>
                                     <?php } ?>
                                 </div>
                                 <div class="media-body">
-                                    <h4 class="media-heading"><a href="{{ url('blog/'.$blog->blogid) }}" target="_blank">{{ $blog->blog_title }}</a></h4>
-                                    <p class="media-content">@php echo strip_tags(str_limit($blog->blog_description, 200 )) @endphp</p>
+                                    <h4 class="media-heading"><a href="{{ url('blogs/'.$blog->blogid) }}" target="_blank">{{ $blog->blog_title }}</a></h4>
+                                    <a href="{{ url('blogs/'.$blog->blogid) }}" target="_blank">
+                                    <p class="media-content">@php echo strip_tags(str_limit($blog->blog_description, 200 )) @endphp</p></a>
                                     <?php
                                        /* $genre_name= "";
                                         if(!empty($blog->blog_genre)){
