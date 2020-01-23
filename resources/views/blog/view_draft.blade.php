2407833dfc3ed506640dd5d239a098bca047a95f
chaitrali <chaitrali@addvector.com>
2020-01-23 23:01:23 +0530

    draft unhandled exception

--- resources/views/blog/view_draft.blade.php
+++ resources/views/blog/view_draft.blade.php
@@ -109,9 +109,9 @@
                             <div class="media" data-val="<?php echo $blog->blogid; ?>">
                                 <div class="media-left">
                                     <?php if (isset($blog->blog_image) && $blog->blog_image != null && file_exists(public_path() . '/blog_img/' . $blog->blog_image)) { ?>
-                                        <a href="{{ url('blog/'.$blog->blogid) }}" target="_blank"><img src="{{ asset("/public/blog_img/".$blog->blog_image) }}" class="media-object"></a>
+                                        <a href="{{ url('blogs/'.$blog->blogid) }}" target="_blank"><img src="{{ asset("/public/blog_img/".$blog->blog_image) }}" class="media-object"></a>
                                     <?php } else { ?>
-                                        <a href="{{ url('blog/'.$blog->blogid) }}" target="_blank"><img src="{{ asset('/') }}public/blog_img/no_img.jpg" class="media-object"></a>
+                                        <a href="{{ url('blogs/'.$blog->blogid) }}" target="_blank"><img src="{{ asset('/') }}public/blog_img/no_img.jpg" class="media-object"></a>
                                     <?php } ?>
                                 </div>
                                 <?php
@@ -123,7 +123,7 @@
                                     ?>
                                 <div class="media-body">
 
-                                    <h4 class="media-heading"><a target="_blank" href="{{ url('blog/'.$blog->blog_slug) }}">{{ $blog->blog_title }}</a></h4>
+                                    <h4 class="media-heading"><a target="_blank" href="{{ url('blogs/'.$blog->blogid) }}">{{ $blog->blog_title }}</a></h4>
                                     <?php $content = preg_replace("/<img[^>]+\>/i", " ", $blog->blog_description); 
                                         $content = preg_replace("/<video[^>]+\>/i", " ", $content); 
                                         $content = str_replace("Your browser does not support HTML5 video.",'', $content); 
